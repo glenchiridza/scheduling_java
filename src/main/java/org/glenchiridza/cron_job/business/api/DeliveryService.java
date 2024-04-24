3508888643023f71e05a33713032fb6dee7f8967
glenchiridza <glenchiridz@gmail.com>
2024-04-24 22:25:44 +0200

    Delivery service

--- src/main/java/org/glenchiridza/cron_job/business/api/DeliveryService.java
+++ src/main/java/org/glenchiridza/cron_job/business/api/DeliveryService.java
@@ -1,5 +1,9 @@
 package org.glenchiridza.cron_job.business.api;
 
+import org.glenchiridza.cron_job.model.MSDelivery;
+
+import java.util.List;
+
 public interface DeliveryService {
 
     List<MSDelivery> getRecentDeliveries();
