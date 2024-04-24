1db4209004c2c1a72569f229077428f48f894ba0
glenchiridza <glenchiridz@gmail.com>
2024-04-24 22:06:13 +0200

    cron initial

---
+++ src/main/java/org/glenchiridza/cron_job/CronJobApplication.java
@@ -0,0 +1,13 @@
+package org.glenchiridza.cron_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/glenchiridza/cron_job/CronJobApplicationTests.java
@@ -0,0 +1,13 @@
+package org.glenchiridza.cron_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
