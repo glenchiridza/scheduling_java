1bf2cf6a360e85a6191b4fbc96104f87f3b8147e
glenchiridza <glenchiridz@gmail.com>
2024-04-25 22:31:23 +0200

    properties yml

--- src/main/java/org/glenchiridza/cron_job/dbconn/impl/MySqlDbConnectionImpl.java
+++ src/main/java/org/glenchiridza/cron_job/dbconn/impl/MySqlDbConnectionImpl.java
@@ -1,4 +1,21 @@
 package org.glenchiridza.cron_job.dbconn.impl;
 
-public class MySqlDbConnectionImpl {
+import org.glenchiridza.cron_job.dbconn.api.MySqlDbConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+public class MySqlDbConnectionImpl implements MySqlDbConnection {
+
+    public static Connection connection;
+
+    @Override
+    public ResultSet executeQuery(String sql) {
+        return null;
+    }
+
+    @Override
+    public Connection openConnection() {
+        return null;
+    }
 }
