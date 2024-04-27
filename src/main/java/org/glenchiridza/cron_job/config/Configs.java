39206facbdc1def61938995855e6146167fb902a
glenchiridza <glenchiridz@gmail.com>
2024-04-27 15:38:40 +0200

    db connection beans

--- src/main/java/org/glenchiridza/cron_job/config/Configs.java
+++ src/main/java/org/glenchiridza/cron_job/config/Configs.java
@@ -2,12 +2,26 @@ package org.glenchiridza.cron_job.config;
 
 import org.glenchiridza.cron_job.business.api.DeliveryService;
 import org.glenchiridza.cron_job.business.impl.DeliveryServiceImpl;
+import org.glenchiridza.cron_job.dbconn.api.MySqlDbConnection;
+import org.glenchiridza.cron_job.dbconn.api.PostgreDbConnection;
+import org.glenchiridza.cron_job.dbconn.impl.MySqlDbConnectionImpl;
+import org.glenchiridza.cron_job.dbconn.impl.PostgreDbConnectionImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configs {
+
+    @Bean
+    PostgreDbConnection postgreDbConnection(){
+        return new PostgreDbConnectionImpl();
+    }
+
+    @Bean
+    MySqlDbConnection mySqlDbConnection(){
+        return new MySqlDbConnectionImpl();
+    }
 
     @Bean
     DeliveryService deliveryService(final ApplicationContext context){
