31f17ad91097c2502f884020b00640be787cc945
glenchiridza <glenchiridz@gmail.com>
2024-04-27 17:22:27 +0200

    Retrieve from postgres database

--- src/main/java/org/glenchiridza/cron_job/business/impl/DeliveryServiceImpl.java
+++ src/main/java/org/glenchiridza/cron_job/business/impl/DeliveryServiceImpl.java
@@ -6,9 +6,13 @@ import org.glenchiridza.cron_job.dbconn.api.MySqlDbConnection;
 import org.glenchiridza.cron_job.dbconn.api.PostgreDbConnection;
 import org.glenchiridza.cron_job.model.MSDelivery;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
 import org.springframework.context.ApplicationContext;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -17,6 +21,9 @@ public class DeliveryServiceImpl implements DeliveryService {
 
     private static final String UPDATE_STATEMENT = "UPDATE";
     private final AtomicBoolean isBusy = new AtomicBoolean(false);
+
+    @Value("${mysql-db.query.delivery}")
+    private String delivery;
 
     @Value("${mysql-db.query.SQL_RETRIEVE_LAST_RUN}")
     private String lastDeliveryTime;
@@ -33,7 +40,46 @@ public class DeliveryServiceImpl implements DeliveryService {
 
     @Override
     public List<MSDelivery> getRecentDeliveries() {
-        return null;
+        List<MSDelivery> deliveries = new ArrayList<>();
+        String lastRunDateTime = getLastRunMysqlDB();
+        String query;
+        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        log.info("last run on mysqldb: {}",lastRunDateTime);
+        if(!lastRunDateTime.isEmpty()){
+            query = delivery.replace("lastRunDateTime",lastDeliveryTime)
+                    .replace("current_date",currentTime);
+        }else{
+            //on start run, in case the last run date-time table has nothing on first run, start from the time just when the db was first created
+            String oldTime = LocalDateTime.of(2023,1,1,0,2).toString();
+            query = delivery.replace("lastRunDateTime",oldTime)
+                    .replace("current_date",currentTime);
+        }
+
+        try{
+            ResultSet resultSet = postgreDbConnection.executeQuery(query);
+            if(resultSet == null){
+                log.info("query return 0 deliveries");
+            }else{
+                log.info("found new deliveries");
+                while (resultSet.next()){
+                    MSDelivery msDelivery = new MSDelivery();
+                    msDelivery.setId(resultSet.getInt(""));
+                    msDelivery.setClientId(resultSet.getInt(""));
+                    msDelivery.setDeliveryCompany(resultSet.getString(""));
+                    msDelivery.setDeliveryCompany(resultSet.getString(""));
+                    msDelivery.setCreatedAt(resultSet.getTimestamp(2).toLocalDateTime());
+
+                    deliveries.add(msDelivery);
+                    log.info("deliveries :: {}",deliveries);
+                }
+
+            }
+        }catch (SQLException ex){
+            return null;
+        }
+
+        return deliveries;
     }
 
     @Override
