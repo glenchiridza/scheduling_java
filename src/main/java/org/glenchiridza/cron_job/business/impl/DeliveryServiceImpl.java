207d49bb342dceb1417680508dcd7004ba993935
glenchiridza <glenchiridz@gmail.com>
2024-04-27 15:32:07 +0200

    delivery service bean

--- src/main/java/org/glenchiridza/cron_job/business/impl/DeliveryServiceImpl.java
+++ src/main/java/org/glenchiridza/cron_job/business/impl/DeliveryServiceImpl.java
@@ -1,4 +1,39 @@
 package org.glenchiridza.cron_job.business.impl;
 
-public class DeliveryServiceImpl {
+import lombok.extern.slf4j.Slf4j;
+import org.glenchiridza.cron_job.business.api.DeliveryService;
+import org.glenchiridza.cron_job.dbconn.api.MySqlDbConnection;
+import org.glenchiridza.cron_job.dbconn.api.PostgreDbConnection;
+import org.glenchiridza.cron_job.model.MSDelivery;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Slf4j
+public class DeliveryServiceImpl implements DeliveryService {
+
+    private static final String UPDATE_STATEMENT = "UPDATE";
+    private final AtomicBoolean isBusy = new AtomicBoolean(false);
+
+    private MySqlDbConnection mySqlDbConnection;
+
+    private PostgreDbConnection postgreDbConnection;
+
+
+    public DeliveryServiceImpl(final ApplicationContext context) {
+        this.postgreDbConnection = context.getBean(PostgreDbConnection.class);
+        this.mySqlDbConnection = context.getBean(MySqlDbConnection.class);
+    }
+
+    @Override
+    public List<MSDelivery> getRecentDeliveries() {
+        return null;
+    }
+
+    @Override
+    public void getCurrentTime(String sql) {
+
+    }
 }
--- src/main/java/org/glenchiridza/cron_job/config/Configs.java
+++ src/main/java/org/glenchiridza/cron_job/config/Configs.java
@@ -1,8 +1,17 @@
 package org.glenchiridza.cron_job.config;
 
+import org.glenchiridza.cron_job.business.api.DeliveryService;
+import org.glenchiridza.cron_job.business.impl.DeliveryServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configs {
+
+    @Bean
+    DeliveryService deliveryService(final ApplicationContext context){
+        return new DeliveryServiceImpl(context);
+    }
 
 }
